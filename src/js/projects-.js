f1caa604e83dba578632202aae0469f9ba6a53aa
Paulo Turco <pauloturcko@gmail.com>
2024-09-10 16:55:05 -0300

    projects visual finished

---
+++ src/js/projects-.js
@@ -0,0 +1,18 @@
+const projects = {
+  "voice-recognition-api": {
+    title: "Voice Recognition API",
+    description:
+      "Este é um projeto que integra reconhecimento de voz em um jogo, ao iniciar, o computador gera um número aleatório entre 1 e 1000, o objetivo dos jogadores é acertar este número secreto. Ao jogador falar algum número, o computador irá reconhecer o que o jogador falou através de uma API de reconhecimento de voz, irá exibir o número que foi falado na tela e dizer se o número secreto é maior ou menor que o número falado.",
+    viewLink: "https://pauloturcko.github.io/ApiVoiceRecognitionGame/",
+    repoLink: "https://github.com/pauloturcko/ApiVoiceRecognitionGame",
+  },
+  fokus: {
+    title: "Fokus",
+    description:
+      "O FoKus é foi desenvolvido para ajudar usuários a melhorarem seu foco e gerenciamento de tempo durante tarefas. Inspirado na técnica Pomodoro, utilizando JavaScript, HTML, e CSS, o design é simples e minimalista, garantindo uma experiência intuitiva e fácil de usar.",
+    viewLink: "https://pauloturcko.github.io/Focus/",
+    repoLink: "https://github.com/pauloturcko/Focus",
+  },
+};
+
+export default { projects };
